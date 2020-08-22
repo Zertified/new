daa89266944f55ca9a3653ca1947a584d02187c0
Zertified <67156986+Zertified@users.noreply.github.com>
2020-08-22 13:40:38 +0100

    Add files via upload

--- Variables.py
+++ Variables.py
@@ -7,38 +7,54 @@ infectionRate = 0.3
 minPeople = 50
 maxPeople = 200
 amountOfPeople = random.randint(minPeople,maxPeople)
+infectedPeople = 0
 
+prob_infected_someone = 1
+min_prob_masks = 1
+max_prob_masks = 2
 
 minDistance = 1
 maxDistance = 3
 actualDistance = random.randint(minDistance,maxDistance)
 
-time = 16
+days = 30
+hours = 24
 
 
 places = [
-    "market","home","club","office","school","doctor","cinema"
+    "home","club","office","school","doctor","cinema","market"
 ]
 
 random.shuffle(places)
 
 class Person():
-    def __init__(self,name,age):
+    def __init__(self):
         self.state = 0
         self.name = name
         self.age = age
         self.position = home
-        self.infected = False
-        self.wears_masks = True
+        self.infected = infected
 
     def moved(self):
         random.shuffle(places)
         self.position = places[0]
 
-    def check_at_home(self):
-        if position is home:
-            infectionRate = 0
-        else:
-            infectionRate = 0.3
+    
+    def isInfected(self):
+        if infected == True:
+            infectedPeople +=1
+        
+        elif self.state == 1:
+            infectedPeople += 1
+        
+        return infectedPeople  # Amount of infected people
+
+
+class Place():
+    pass
+
+
+
+    
     
         
